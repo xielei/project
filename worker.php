3210914a676197d4117f92020fa8ac83b159407f
xielei <66499598+xielei@users.noreply.github.com>
2021-07-21 17:47:54 +0800

    Create worker.php

---
+++ worker.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Xielei\Swoole\Worker;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$worker = new Worker();
+
+$worker->worker_file = __DIR__ . '/event/worker.php';
+
+$worker->start();
