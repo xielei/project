2afc24dd89fd71af299cd324af9bd277d8ba824a
xielei <66499598+xielei@users.noreply.github.com>
2021-07-21 17:58:38 +0800

    Update gateway.php

--- gateway.php
+++ gateway.php
@@ -8,6 +8,16 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $gateway = new Gateway();
 
+// 配置register连接信息
+$gateway->register_host = '127.0.0.1';
+$gateway->register_port = 9327;
+$gateway->register_secret_key = 'qwertyuiop';
+
+// 设置内部连接地址端口
+$gateway->lan_host = '127.0.0.1';
+$gateway->lan_port = 9108;
+
+// 监听
 $gateway->listen('127.0.0.1', 8000, [
     'open_websocket_protocol' => true,
     'open_websocket_close_frame' => true,
