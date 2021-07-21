69d88624b71d963c5b290485602e08348f26bf53
xielei <66499598+xielei@users.noreply.github.com>
2021-07-21 17:50:25 +0800

    Update task.php

--- event/task.php
+++ event/task.php
@@ -4,4 +4,23 @@ use Xielei\Swoole\Helper\TaskEvent as HelperTaskEvent;
 
 class TaskEvent extends HelperTaskEvent
 {
+    public function onWorkerStart()
+    {
+    }
+
+    public function onWorkerExit()
+    {
+    }
+
+    public function onWorkerStop()
+    {
+    }
+
+    public function onTask(Task $task)
+    {
+    }
+
+    public function onPipeMessage(PipeMessage $pipeMessage)
+    {
+    }
 }
