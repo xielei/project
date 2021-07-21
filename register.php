a0f238a32bd0a4f25396abd15d161697b0b58fcd
xielei <66499598+xielei@users.noreply.github.com>
2021-07-21 17:56:47 +0800

    Update register.php

--- register.php
+++ register.php
@@ -6,6 +6,6 @@ use Xielei\Swoole\Register;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$register = new Register();
+$register = new Register('127.0.0.1', 9327, 'qwertyuiop');
 
 $register->start();
