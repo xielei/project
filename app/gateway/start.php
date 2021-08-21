7100dfcb756acd3028fc893c1c8b43cb55981402
xielei <66499598+xielei@users.noreply.github.com>
2021-08-21 11:07:21 +0800

    Update start.php

--- app/gateway/start.php
+++ app/gateway/start.php
@@ -28,24 +28,24 @@ $gateway->lan_port = 9108;
 // 监听一个裸TCP端口
 $gateway->listen('127.0.0.1', 8000);
 
-// 监听一个自定义协议TCP端口
-$gateway->listen('127.0.0.1', 8001, [
-    'open_eof_split' => true,   //打开EOF_SPLIT检测
-    'package_eof'    => "\r\n", //设置EOF
-]);
+// // 监听一个自定义协议TCP端口
+// $gateway->listen('127.0.0.1', 8001, [
+//     'open_eof_split' => true,   //打开EOF_SPLIT检测
+//     'package_eof'    => "\r\n", //设置EOF
+// ]);
 
-// 监听一个websocket协议端口
-$gateway->listen('127.0.0.1', 8002, [
-    'open_websocket_protocol' => true,
-    'open_websocket_close_frame' => true,
-]);
+// // 监听一个websocket协议端口
+// $gateway->listen('127.0.0.1', 8002, [
+//     'open_websocket_protocol' => true,
+//     'open_websocket_close_frame' => true,
+// ]);
 
-// 监听一个加密websocket协议端口
-$gateway->listen('127.0.0.1', 8003, [
-    'open_websocket_protocol' => true,
-    'open_websocket_close_frame' => true,
-    'ssl_cert_file' => __DIR__ . '/cert/xxx.pem',
-    'ssl_key_file' => __DIR__ . '/cert/xxx.key',
-], SWOOLE_SOCK_TCP | SWOOLE_SSL);
+// // 监听一个加密websocket协议端口
+// $gateway->listen('127.0.0.1', 8003, [
+//     'open_websocket_protocol' => true,
+//     'open_websocket_close_frame' => true,
+//     'ssl_cert_file' => __DIR__ . '/cert/xxx.pem',
+//     'ssl_key_file' => __DIR__ . '/cert/xxx.key',
+// ], SWOOLE_SOCK_TCP | SWOOLE_SSL);
 
 $gateway->start();
