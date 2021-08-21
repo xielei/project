3b1dd6745752cbd3b5a5fb4134019e89ad70e394
xielei <66499598+xielei@users.noreply.github.com>
2021-08-21 17:53:28 +0800

    Update config.php

--- app/worker/config.php
+++ app/worker/config.php
@@ -12,6 +12,9 @@ return [
     // 自动重载监听的文件或目录(配置文件已自动纳入)。
     'reload_watch' => [__DIR__ . '/event/'],
 
+    // 服务标签(供gateway自定义路由选择参考)
+    'tag_list' => [],
+
     // worker事件文件
     'worker_file' => __DIR__ . '/event/worker.php',
 
